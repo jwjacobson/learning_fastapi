97029ae27abc6493da760529f5b435ff0f079561
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-07 18:29:05 -0500

    Add enum instance and corresponding path

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+class Musician(str, Enum):
+    trane = 'trane'
+    mccoy = 'mccoy'
+    elvin = 'elvin'
 
 app = FastAPI()
 
 @app.get('/items/{item_id}')
-async def read_item(item_id):
+async def read_item(item_id: int):
     return {'item_id': item_id}
+
+@app.get('/musicians/{musician}')
+async def get_musician(musician: Musician):
+    if musician is Musician.trane:
+        return {'musician': musician, 'instrument': 'tenor'}
+    
+    if musician.value == 'mccoy':
+        return {'musician': musician, 'instrument': 'piano'}
+
+    return {'musician': musician, 'instrument': drums}
+    
+
