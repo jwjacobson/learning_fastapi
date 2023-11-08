f59f2a6feb5aec2bcbaa959bfce009e6edf499be
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-07 19:04:43 -0500

    Add path with required parameter

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ class Musician(str, Enum):
 
 app = FastAPI()
 
-@app.get('/users/{user_id}/items/{item_id}')
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-    ):
-    item = {'item_id': item_id, "owner_id": user_id}
-    if q:
-        item.update({'q': q})
-    if not short:
-        item.update(
-            {'description': 'ok whatever'}
-        )
+# messy function to show different parameters etc.
+# @app.get('/users/{user_id}/items/{item_id}')
+# async def read_user_item(
+#     user_id: int, item_id: str, q: str | None = None, short: bool = False
+#     ):
+#     item = {'item_id': item_id, "owner_id": user_id}
+#     if q:
+#         item.update({'q': q})
+#     if not short:
+#         item.update(
+#             {'description': 'ok whatever'}
+#         )
+#     return item
+
+@app.get('/items/{item_id}')
+async def read_user_utem(item_id: str, needy: str):
+    item = {'item_id': item_id, 'needy': needy}
     return item
 
 @app.get('/musicians/{musician}')
