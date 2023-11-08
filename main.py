ea16386e68e635ec952729a7b07ef63ad4470b10
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-07 19:02:06 -0500

    Add user parameter to path

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ class Musician(str, Enum):
 
 app = FastAPI()
 
-@app.get('/items/{item_id}')
-async def read_item(item_id: str, q: str | None = None, short: bool = False):
-    item = {'item_id': item_id}
+@app.get('/users/{user_id}/items/{item_id}')
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+    ):
+    item = {'item_id': item_id, "owner_id": user_id}
     if q:
         item.update({'q': q})
     if not short:
