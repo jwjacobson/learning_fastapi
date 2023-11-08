f434bb7a31b366249adf45275f35f843ebbd4bcf
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-08 15:11:26 -0500

    Add put path to illustrate simultaenous path and query parameters

--- requestbody/main.py
+++ requestbody/main.py
@@ -9,10 +9,14 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-@app.post('/items/')
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({'price with tax': price_with_tax})
-    return item_dict
+# @app.post('/items/')
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({'price with tax': price_with_tax})
+#     return item_dict
+
+@app.put('/items/{item_id}')
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
