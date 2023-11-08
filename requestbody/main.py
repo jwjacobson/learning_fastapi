9abdc93c1f1ff54cd4c0dd2a975a5c0d89671567
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-11-08 15:28:10 -0500

    Use Field to annotate and define model attributes

--- requestbody/main.py
+++ requestbody/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title='Description of the item', max_length=200
+    )
+    price: float = Field(gt=0, description='price must be greater than zero')
     tax: float | None = None
 
 app = FastAPI()
@@ -18,8 +20,6 @@ app = FastAPI()
 #     return item_dict
 
 @app.put('/items/{item_id}')
-async def create_item(item_id: int, item: Item, q: str | None = None):
-    result = {'item_id': item_id, **item.dict()}
-    if q:
-        result.update({'q': q})
+async def update_item(item_id: int, item: Item=Body(embed=True)):
+    result = {'item_id': item_id, 'item': item}
     return result
